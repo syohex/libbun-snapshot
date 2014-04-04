libbun/libbun

add python parser

java/src/libbun/lang/python/PythonBlockPatternFunction.java
@@ -1,0 +1,39 @@
+package libbun.lang.python;
+
+import libbun.parser.ZToken;
+import libbun.parser.ZTokenContext;
+import libbun.parser.ast.ZBlockNode;
+import libbun.parser.ast.ZNode;
+import libbun.util.Var;
+import libbun.util.ZMatchFunction;
+
+public class PythonBlockPatternFunction extends ZMatchFunction {
+
+	@Override
+	public ZNode Invoke(ZNode ParentNode, ZTokenContext TokenContext,
+			ZNode LeftNode) {
+		@Var ZNode BlockNode =  new ZBlockNode(ParentNode, ParentNode.GetNameSpace());
+		@Var ZToken SkipToken = TokenContext.GetToken();
+		BlockNode = TokenContext.MatchToken(BlockNode, ":", ZTokenContext._Required);
+		if(!BlockNode.IsErrorNode()) {
+			@Var boolean Remembered = TokenContext.SetParseFlag(ZTokenContext._AllowSkipIndent); // init
+			@Var ZNode NestedBlockNode = BlockNode;
+			@Var int IndentSize = 0;
+			while(TokenContext.HasNext()) {
+				@Var ZToken Token = TokenContext.GetToken();
+				if(IndentSize > Token.GetIndentSize()) {
+					break;
+				}
+				IndentSize = Token.GetIndentSize();
+				NestedBlockNode = TokenContext.MatchPattern(NestedBlockNode, ZNode._NestedAppendIndex, "$Statement$", ZTokenContext._Required);
+				if(NestedBlockNode.IsErrorNode()) {
+					TokenContext.SkipError(SkipToken);
+					break;
+				}
+			}
+			TokenContext.SetParseFlag(Remembered);
+		}
+		return BlockNode;
+	}
+
+}

java/src/libbun/lang/python/PythonFunctionPatternFunction.java
@@ -1,0 +1,20 @@
+package libbun.lang.python;
+
+import libbun.parser.ZTokenContext;
+import libbun.parser.ast.ZFunctionNode;
+import libbun.parser.ast.ZNode;
+import libbun.util.Var;
+import libbun.util.ZMatchFunction;
+
+public class PythonFunctionPatternFunction extends ZMatchFunction {
+    @Override
+    public ZNode Invoke(ZNode ParentNode, ZTokenContext TokenContext, ZNode LeftNode) {
+        @Var ZNode FuncNode = new ZFunctionNode(ParentNode);
+        FuncNode = TokenContext.MatchToken(FuncNode, "def", ZTokenContext._Required);
+        FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._NameInfo, "$Name$", ZTokenContext._Optional);
+        FuncNode = TokenContext.MatchNtimes(FuncNode, "(", "$Param$", ",", ")");
+        FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._TypeInfo, "$TypeAnnotation$", ZTokenContext._Optional);
+        FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._Block, "$Block$", ZTokenContext._Required);
+        return FuncNode;
+    }
+}

java/src/libbun/lang/python/PythonIfPatternFunction.java
@@ -1,0 +1,22 @@
+package libbun.lang.python;
+
+import libbun.parser.ZTokenContext;
+import libbun.parser.ast.ZIfNode;
+import libbun.parser.ast.ZNode;
+import libbun.util.Var;
+import libbun.util.ZMatchFunction;
+
+public class PythonIfPatternFunction extends ZMatchFunction{
+	@Override
+	public ZNode Invoke(ZNode ParentNode, ZTokenContext TokenContext, ZNode LeftNode) {
+		@Var ZNode IfNode = new ZIfNode(ParentNode);
+		IfNode = TokenContext.MatchToken(IfNode, "if", ZTokenContext._Required);
+		IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Cond, "$Expression$", ZTokenContext._Required, ZTokenContext._AllowNewLine);
+		IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Then, "$Block$", ZTokenContext._Required);
+		//FIXME elif
+		if(TokenContext.MatchNewLineToken("else")) {
+			IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Else, "$Block$", ZTokenContext._Required);
+		}
+		return IfNode;
+	}
+}

syohex/libbun-snapshot

java/src/libbun/lang/python/PythonBlockPatternFunction.java
@@ -1,0 +1,39 @@
+package libbun.lang.python;
+
+import libbun.parser.BToken;
+import libbun.parser.BTokenContext;
+import libbun.parser.ast.BNode;
+import libbun.parser.ast.ZBlockNode;
+import libbun.util.BMatchFunction;
+import libbun.util.Var;
+
+public class PythonBlockPatternFunction extends BMatchFunction {
+
+	@Override
+	public BNode Invoke(BNode ParentNode, BTokenContext TokenContext,
+			BNode LeftNode) {
+		@Var BNode BlockNode =  new ZBlockNode(ParentNode, ParentNode.GetNameSpace());
+		@Var BToken SkipToken = TokenContext.GetToken();
+		BlockNode = TokenContext.MatchToken(BlockNode, ":", BTokenContext._Required);
+		if(!BlockNode.IsErrorNode()) {
+			@Var boolean Remembered = TokenContext.SetParseFlag(BTokenContext._AllowSkipIndent); // init
+			@Var BNode NestedBlockNode = BlockNode;
+			@Var int IndentSize = 0;
+			while(TokenContext.HasNext()) {
+				@Var BToken Token = TokenContext.GetToken();
+				if(IndentSize > Token.GetIndentSize()) {
+					break;
+				}
+				IndentSize = Token.GetIndentSize();
+				NestedBlockNode = TokenContext.MatchPattern(NestedBlockNode, BNode._NestedAppendIndex, "$Statement$", BTokenContext._Required);
+				if(NestedBlockNode.IsErrorNode()) {
+					TokenContext.SkipError(SkipToken);
+					break;
+				}
+			}
+			TokenContext.SetParseFlag(Remembered);
+		}
+		return BlockNode;
+	}
+
+}

java/src/libbun/lang/python/PythonFunctionPatternFunction.java
@@ -1,0 +1,20 @@
+package libbun.lang.python;
+
+import libbun.parser.BTokenContext;
+import libbun.parser.ast.BNode;
+import libbun.parser.ast.ZFunctionNode;
+import libbun.util.BMatchFunction;
+import libbun.util.Var;
+
+public class PythonFunctionPatternFunction extends BMatchFunction {
+	@Override
+	public BNode Invoke(BNode ParentNode, BTokenContext TokenContext, BNode LeftNode) {
+		@Var BNode FuncNode = new ZFunctionNode(ParentNode);
+		FuncNode = TokenContext.MatchToken(FuncNode, "def", BTokenContext._Required);
+		FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._NameInfo, "$Name$", BTokenContext._Optional);
+		FuncNode = TokenContext.MatchNtimes(FuncNode, "(", "$Param$", ",", ")");
+		FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._TypeInfo, "$TypeAnnotation$", BTokenContext._Optional);
+		FuncNode = TokenContext.MatchPattern(FuncNode, ZFunctionNode._Block, "$Block$", BTokenContext._Required);
+		return FuncNode;
+	}
+}

java/src/libbun/lang/python/PythonIfPatternFunction.java
@@ -1,0 +1,22 @@
+package libbun.lang.python;
+
+import libbun.parser.BTokenContext;
+import libbun.parser.ast.BNode;
+import libbun.parser.ast.ZIfNode;
+import libbun.util.BMatchFunction;
+import libbun.util.Var;
+
+public class PythonIfPatternFunction extends BMatchFunction{
+	@Override
+	public BNode Invoke(BNode ParentNode, BTokenContext TokenContext, BNode LeftNode) {
+		@Var BNode IfNode = new ZIfNode(ParentNode);
+		IfNode = TokenContext.MatchToken(IfNode, "if", BTokenContext._Required);
+		IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Cond, "$Expression$", BTokenContext._Required, BTokenContext._AllowNewLine);
+		IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Then, "$Block$", BTokenContext._Required);
+		//FIXME elif
+		if(TokenContext.MatchNewLineToken("else")) {
+			IfNode = TokenContext.MatchPattern(IfNode, ZIfNode._Else, "$Block$", BTokenContext._Required);
+		}
+		return IfNode;
+	}
+}